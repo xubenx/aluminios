a533fa7be679c4b6fd4b323160fd51c081d92e20
xubenx <xubenx@2806-103e-001a-60f2-6ef6-daff-fee0-9a6b.ipv6.infinitum.net.mx>
2025-04-09 15:29:18 -0600

    Alu Co

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  distDir: "build",
-  reactStrictMode: true,
-  swcMinify: true,
-  
+
   
 };
 
